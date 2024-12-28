53524dac005443f8650304b5a147d00e41d9353e
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2024-12-28 18:19:04 -0300

    update lógica de geração de contrato

--- public/js/adm/reserva.js
+++ public/js/adm/reserva.js
@@ -1,12 +1,14 @@
 function updatePagamento(idReserva) {
-    $.ajax({
-        'processing': true, 
-        'serverSide': false,
-          type: "GET",
-          data: {id: idReserva},
-          url: "/qsb/public/gerar-contrato",
-          success: function() {
-            alert('Contrato gerado com sucesso');
-          }
-      });
-}
+    if(confirm('Ao gerar o contrato você confirma o pagamento. Deseja continuar?')){
+     $.ajax({
+         'processing': true, 
+         'serverSide': false,
+           type: "GET",
+           data: {id: idReserva},
+           url: "/qsb/public/gerar-contrato",
+           success: function() {
+             alert('Contrato gerado com sucesso');
+           }
+       });
+    }
+ } 

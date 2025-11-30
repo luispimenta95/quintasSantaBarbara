cc9c033795a270658ffc9343f9e520f3c4210f09
luispimenta <luisfelipearaujopimenta@gmail.com>
2025-11-30 15:46:30 -0300

    Add Portuguese localization for jQuery UI Datepicker

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -8,6 +8,29 @@
     <!-- Incluir o jQuery UI -->
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <script>
+        /* Configuração regional do jQuery UI Datepicker para português (pt-BR) */
+        if (typeof $.datepicker !== 'undefined') {
+            $.datepicker.regional['pt-BR'] = {
+                closeText: 'Fechar',
+                prevText: 'Anterior',
+                nextText: 'Próximo',
+                currentText: 'Hoje',
+                monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+                monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+                dayNames: ['domingo','segunda-feira','terça-feira','quarta-feira','quinta-feira','sexta-feira','sábado'],
+                dayNamesShort: ['dom','seg','ter','qua','qui','sex','sáb'],
+                dayNamesMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb'],
+                weekHeader: 'Sm',
+                dateFormat: 'dd/mm/yy',
+                firstDay: 0,
+                isRTL: false,
+                showMonthAfterYear: false,
+                yearSuffix: ''
+            };
+            $.datepicker.setDefaults($.datepicker.regional['pt-BR']);
+        }
+    </script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="{{ asset('js/hospedes/cadastro.js') }}" defer></script>
     <!-- Adicionando o JS do Bootstrap (com dependência do jQuery e Popper.js) -->
